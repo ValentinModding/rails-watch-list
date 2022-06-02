b31f25cfb94f736a74e51c6067687136cef725c7
Valentin Marcoux <valentinmarcoux307@gmail.com>
2022-06-02 13:07:25 +0200

    Ajout des seeds

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,5 +2,5 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
   validates :comment, length: { minimum: 6 }
-  validates :movie_id, uniqueness: { scope: :list_id, message: "One List per movie" }
+  validates :movie_id, uniqueness: { scope: :list_id, message: 'One list per movie' }
 end
